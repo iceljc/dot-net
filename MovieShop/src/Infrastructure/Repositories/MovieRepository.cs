c647c30623d470c0f7b71514c2b7b25acd2aacfa
iceljc <franklujc@gmail.com>
2021-04-09 20:47:16 -0500

    init

---
+++ MovieShop/src/Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using ApplicationCore.Exceptions;
+using ApplicationCore.Helpers;
+using ApplicationCore.RepositoryInterfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class MovieRepository : EfRepository<Movie>, IMovieRepository
+    {
+        public MovieRepository(MovieShopDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Movie>> GetTopRatedMovies()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<PaginatedList<Movie>> GetMoviesByGenre(int genreId, int pageSize = 25, int page = 1)
+        {
+            var totalMoviesCountByGenre =
+                await _dbContext.Genres.Include(g => g.Movies).Where(g => g.Id == genreId).SelectMany(g => g.Movies)
+                    .CountAsync();
+
+            if (totalMoviesCountByGenre == 0)
+            {
+                throw new NotFoundException("NO Movies found for this genre");
+            }
+            var movies = await _dbContext.Genres.Include(g => g.Movies).Where(g => g.Id == genreId)
+                .SelectMany(g => g.Movies)
+                .OrderByDescending(m => m.Revenue).Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PaginatedList<Movie>(movies, totalMoviesCountByGenre, page, pageSize);
+        }
+
+        public async Task<IEnumerable<Movie>> GetHighestGrossingMovies()
+        {
+            var movies = await _dbContext.Movies.OrderByDescending(m => m.Revenue).Take(24).ToListAsync();
+            return movies;
+        }
+
+        public async Task<IEnumerable<Review>> GetMovieReviews(int id)
+        {
+            var reviews = await _dbContext.Reviews.Where(r => r.MovieId == id).Include(r => r.User)
+                .Select(r => new Review
+                {
+                    UserId = r.UserId,
+                    Rating = r.Rating,
+                    MovieId = r.MovieId,
+                    ReviewText = r.ReviewText,
+                    User = new User
+                    {
+                        Id = r.UserId,
+                        FirstName = r.User.FirstName,
+                        LastName = r.User.LastName
+                    }
+                }).Take(10).ToListAsync();
+            return reviews;
+        }
+
+        public override async Task<Movie> GetByIdAsync(int id)
+        {
+            var movie = await _dbContext.Movies.Include(m => m.MovieCasts).ThenInclude(m => m.Cast)
+                .Include(m => m.Genres)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                throw new NotFoundException("Movie Not found");
+            }
+
+            var movieRating = await _dbContext.Reviews.Where(r => r.MovieId == id).DefaultIfEmpty()
+                .AverageAsync(r => r == null ? 0 : r.Rating);
+            if (movieRating > 0) movie.Rating = movieRating;
+
+            return movie;
+        }
+    }
+}
---
+++ MovieShop/src/MovieShop.API/Startup.cs
@@ -0,0 +1,78 @@
+using Infrastructure.Data;
+using Infrastructure.Filters;
+using Infrastructure.Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace MovieShop.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<MovieShopLogFilter>();
+
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new MovieShopHeader("Company", "Movie Shop Inc."));
+                options.Filters.Add(typeof(MovieShopLogFilter)); // By type
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "MovieShop.API", Version = "v1"});
+            });
+            services.AddDbContext<MovieShopDbContext>(options =>
+                options.UseSqlServer(Configuration
+                    .GetConnectionString("MovieShopDbConnection")));
+            services.AddAutoMapper(typeof(Startup), typeof(MovieShopMappingProfile));
+            ConfigureDependencyInjection(services);
+            services.AddMemoryCache();
+            services.AddHttpContextAccessor();
+        }
+
+        private void ConfigureDependencyInjection(IServiceCollection services)
+        {
+            services.AddRepositories();
+            services.AddServices();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieShop.API v1"));
+            }
+
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins(Configuration.GetValue<string>("clientSPAUrl")).AllowAnyHeader()
+                    .AllowAnyMethod().AllowCredentials();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
